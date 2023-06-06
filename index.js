ac9ad4980c379aab19c796e67d4fb67201596149
Tou Xiong <toufxiong83@gmail.com>
2023-06-05 21:11:29 -0700

    -removed scrolling action from index.js

--- index.js
+++ index.js
@@ -8,80 +8,91 @@ body.overflow = "hidden";
 let box = document.getElementsByClassName("container-01");
 for (let i = 0; i < box.length; i++) {
   box[i].style.display = "flex";
-  box[i].style.border = "1px solid #fefefe";
   box[i].style.flexDirection = "column";
   box[i].style.paddingLeft = "1rem";
+  box[i].style.width = "75%";
+  box[i].style.marginLeft = "auto";
+  box[i].style.marginRight = "auto";
+}
+
+let box2 = document.getElementsByClassName("container-02");
+for (let i = 0; i < box2.length; i++) {
+  //box2[i].style.border = "1px solid #fefefe";
+  box2[i].style.width = "80%";
+  box2[i].style.marginLeft = "auto";
+  box2[i].style.marginRight = "auto";
+  box2[i].style.paddingLeft = "5rem";
 }
 
 // loop through all the images to apply effects
 let images = document.querySelectorAll("img");
 for (let i = 0; i < images.length; i++) {
-  images[i].style.width = "40vmin";
-  images[i].style.height = "40vmim";
+  images[i].style.width = "20vmin";
+  images[i].style.height = "20vmim";
   images[i].style.objectFit = "cover";
   images[i].style.objectPosition = "center";
 }
 
-const imageTrack = document.getElementById("images-track");
-imageTrack.style.display = "flex";
-imageTrack.style.gap = "4vmin";
-imageTrack.style.position = "absolute";
-imageTrack.style.left = "50%";
-imageTrack.style.top = "50%";
-imageTrack.style.transform = "translate(0%, -50%)";
-imageTrack.style.marginTop = "10rem";
+//const imageTrack = document.getElementById("images-track");
+//imageTrack.style.display = "flex";
+//imageTrack.style.gap = "4vmin";
+//imageTrack.style.position = "absolute";
+//imageTrack.style.left = "50%";
+//imageTrack.style.top = "50%";
+//imageTrack.style.transform = "translate(0%, -50%)";
+//imageTrack.style.marginTop = "10rem";
 
 // Code below is from https://codepen.io/Hyperplexed/full/MWXBRBp
 // It allows the images to be scrolled across the screen
-const track = document.getElementById("images-track");
+//const track = document.getElementById("images-track");
 
-const handleOnDown = (e) => (track.dataset.mouseDownAt = e.clientX);
+//const handleOnDown = (e) => (track.dataset.mouseDownAt = e.clientX);
 
-const handleOnUp = () => {
-  track.dataset.mouseDownAt = "0";
-  track.dataset.prevPercentage = track.dataset.percentage;
-};
+//const handleOnUp = () => {
+//track.dataset.mouseDownAt = "0";
+//track.dataset.prevPercentage = track.dataset.percentage;
+//};
 
-const handleOnMove = (e) => {
-  if (track.dataset.mouseDownAt === "0") return;
+//const handleOnMove = (e) => {
+//if (track.dataset.mouseDownAt === "0") return;
 
-  const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
-    maxDelta = window.innerWidth / 2;
+//const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
+//maxDelta = window.innerWidth / 2;
 
-  const percentage = (mouseDelta / maxDelta) * -100,
-    nextPercentageUnconstrained =
-      parseFloat(track.dataset.prevPercentage) + percentage,
-    nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
+//const percentage = (mouseDelta / maxDelta) * -100,
+//nextPercentageUnconstrained =
+//parseFloat(track.dataset.prevPercentage) + percentage,
+//nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
 
-  track.dataset.percentage = nextPercentage;
+//track.dataset.percentage = nextPercentage;
 
-  track.animate(
-    {
-      transform: `translate(${nextPercentage}%, -50%)`,
-    },
-    { duration: 1200, fill: "forwards" }
-  );
+//track.animate(
+//{
+//transform: `translate(${nextPercentage}%, -50%)`,
+//},
+//{ duration: 1200, fill: "forwards" }
+//);
 
-  for (const image of track.getElementsByClassName("image")) {
-    image.animate(
-      {
-        objectPosition: `${100 + nextPercentage}% center`,
-      },
-      { duration: 1200, fill: "forwards" }
-    );
-  }
-};
+//for (const image of track.getElementsByClassName("image")) {
+//image.animate(
+//{
+//objectPosition: `${100 + nextPercentage}% center`,
+//},
+//{ duration: 1200, fill: "forwards" }
+//);
+//}
+//};
 
-/* -- Had to add extra lines for touch events -- */
+///* -- Had to add extra lines for touch events -- */
 
-window.onmousedown = (e) => handleOnDown(e);
+//window.onmousedown = (e) => handleOnDown(e);
 
-window.ontouchstart = (e) => handleOnDown(e.touches[0]);
+//window.ontouchstart = (e) => handleOnDown(e.touches[0]);
 
-window.onmouseup = (e) => handleOnUp(e);
+//window.onmouseup = (e) => handleOnUp(e);
 
-window.ontouchend = (e) => handleOnUp(e.touches[0]);
+//window.ontouchend = (e) => handleOnUp(e.touches[0]);
 
-window.onmousemove = (e) => handleOnMove(e);
+//window.onmousemove = (e) => handleOnMove(e);
 
-window.ontouchmove = (e) => handleOnMove(e.touches[0]);
+//window.ontouchmove = (e) => handleOnMove(e.touches[0]);
