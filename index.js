62b599b1dd43caca74b84b784efec7d7f4f55d28
Tou Xiong <toufxiong83@gmail.com>
2023-06-06 22:21:41 -0700

    replaced "div" with correct semantics. renamed contact.js to my_projects.js

--- index.js
+++ index.js
@@ -17,11 +17,19 @@ for (let i = 0; i < box.length; i++) {
 
 let box2 = document.getElementsByClassName("container-02");
 for (let i = 0; i < box2.length; i++) {
-  //box2[i].style.border = "1px solid #fefefe";
-  box2[i].style.width = "80%";
+  box2[i].style.display = "flex";
+  box2[i].style.flexDirection = "column";
+  box2[i].style.paddingLeft = "1rem";
+  box2[i].style.width = "75%";
   box2[i].style.marginLeft = "auto";
   box2[i].style.marginRight = "auto";
-  box2[i].style.paddingLeft = "5rem";
+}
+let image = document.getElementById("images-track");
+for (let i = 0; i < image.length; i++) {
+  image[i].style.width = "80%";
+  image[i].style.marginLeft = "auto";
+  image[i].style.marginRight = "auto";
+  image[i].style.paddingLeft = "5rem";
 }
 
 // loop through all the images to apply effects
